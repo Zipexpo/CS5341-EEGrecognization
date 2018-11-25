7676e62599df38e7836942a3568ff233e25e3b86
Zipexpo <nvtngan@hcmus.edu.vn>
2018-11-24 21:53:02 -0800

    updata

--- convertTreeclass2Flatclass.m
+++ convertTreeclass2Flatclass.m
@@ -1,6 +1,24 @@
-function a = convertTreeclass2Flatclass(data)
-[n,va] = size();
+function [a] = convertTreeclass2Flatclass(data)
+a = data;
+[n,va] = size(a);
 for i = 1:n
-    a
+    if isundefined(a.class4(i))
+        if isundefined(a.class3(i))
+            if isundefined(a.class2(i))
+                a.mainclass(i) = a.class1(i);
+            else
+                a.mainclass(i) = a.class2(i);
+            end
+        
+        else
+            a.mainclass(i) = a.class3(i);
+        end
+    else
+        a.mainclass(i) = a.class4(i);
+    end
 end
+a.class1 =[];
+a.class2 =[];
+a.class3 =[];
+a.class4 =[];
 end
