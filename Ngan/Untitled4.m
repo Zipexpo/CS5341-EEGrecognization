9e7cee8fdb626fffcbc0935c638ee68b46aa7c8c
Zipexpo <nvtngan@hcmus.edu.vn>
2018-12-01 11:09:54 -0600

    update N

--- Ngan/Untitled4.m
+++ Ngan/Untitled4.m
@@ -1,7 +1,29 @@
+%loopsetting = 10:100:2000;
+loopsetting = [10:100:2000 2200:200:5000];
+LearningCyclessetting = 10:10:50;
+count = 1;
+count2 = 1;
+testingAccuracy = zeros(length(loopsetting),length(LearningCyclessetting));
+timeper = testingAccuracy;
+validationAccuracy = testingAccuracy;
+for i = loopsetting
+    for j = LearningCyclessetting
+        tic
+        [trainedClassifier, validationAccuracy(count,count2)] = baggedtree(traindata,[i 30]);
+        timeper(count,count2) = toc;
+        results = trainedClassifier.predictFcn(testdata);
+        racc = sum(results == testdata.mainclass);
+        sizetest = size(results);
+        sizetest = sizetest(1);
+        testingAccuracy(count,count2) = racc/sizetest;
+        count2 = count2+1;
+    end
+    count2 =1;
+    count = count+1;
+end
+save('result.mat', 'timeper', 'testingAccuracy','validationAccuracy','loopsetting','LearningCyclessetting');
 
 
-results = trainedModel.predictFcn(testingdata);
-racc = sum(results == testingdata.mainclass);
-sizetest = size(results);
-sizetest = sizetest(1);
-accuracy = racc/sizetest;
+%% 
+figure(1)
+plot(loopsetting,validationAccuracy)
