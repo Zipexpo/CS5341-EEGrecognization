71fb4aaa45167bf1f754c4f339c6cf3b2120855d
nnhuyen <huyen.nguyen@ttu.edu>
2018-12-03 14:30:52 -0600

    Update result

--- Huyen/run.m
+++ Huyen/run.m
@@ -22,18 +22,67 @@ net.layers{2}.transferFcn = 'poslin';
 
 %  ================
 
-net = patternnet([10,10]);
-net.layers{1}.transferFcn = 'poslin';
+net = patternnet([50,50,50]);
+net.trainParam.max_fail = 50;
+net.trainParam.epochs = 2000;
 net.divideFcn = 'divideind';
-net.divideParam.trainInd = (1:1406);
-net.divideParam.valInd = (1407:1757);
-net.divideParam.testInd = (1758:2196);
+net.divideParam.trainInd = 1:1406;
+net.divideParam.valInd = 1407:1757;
+net.divideParam.testInd = 1758:2196;
 [net,tr] = train(net,newInput,newTarget);
 
 
 
+outArray = zeros(5,6);
 
 
+
+maxcount = 0;
+trn = 0;
+finalout = 0;
+
+for i=1:10
+
+    count = 0;
+    net = patternnet([60,60,60]);
+    net.trainParam.max_fail = 50;
+    net.trainParam.epochs = 2000;
+    net.divideFcn = 'divideind';
+    net.divideParam.trainInd = 1:1406;
+    net.divideParam.valInd = 1407:1757;
+    net.divideParam.testInd = 1758:2196;
+    net.trainParam.epochs = 5000;
+    net.layers{1}.transferFcn = 'poslin';
+    net.layers{2}.transferFcn = 'poslin';
+    [net,tr] = train(net,newInput,newTarget);
+    outputs = net(newInput);
+    
+    [u,v] = max(outputs);
+    out = zeros(a,b);
+    for j = 1:b
+        out(v(j),j) = 1;
+        
+    end
+
+     for k = 1758:2196
+         for l = 1:a
+             if (out(l,k)==newTarget(l,k)) && (newTarget(l,k)==1)
+                 count = count +1;
+             end
+         end
+     end
+%     count = sum(out==newTarget);
+    if (count > maxcount)
+         maxcount = count;
+        finalout = net(newInput);
+        trn = tr;
+        fprintf("Hi");
+    end
+end
+
+figure, plotconfusion(newTarget(:,1758:2196),finalout(:,1758:2196))
+finalout__  =finalout
+tr22 = trn
 
 
 
